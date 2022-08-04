cdba167b223aec2d62113ce7b5f79d2e3e8d04ad
Jinshuai Gan <1179388495.JG@gmail.com>
2022-08-04 13:33:30 +0800

    This is some Python study notes

--- Python_list.py
+++ Python_list.py
@@ -170,16 +170,117 @@ names_python_pc_2.sort(key=len)
 print('根据元素长度排序后的结果是：', names_python_pc_2)
 
 
+# 临时排序，创建新的列表
+temp_list = sorted(list_1)
+print('临时排序', temp_list)
+print('原来的列表元素顺序', list_1)
 
 
+'''
+在 Python 编程中，我们经常使用 in 和 not in 来判断一个元素是否在列表中。如果满足
+则返回 True,否则返回 False。
+'''
+# 使用 in 判断一个元素是否在列表中
+names_python_pc = ['陈升', '刘德华', '杨幂', 'TFboys']
+in_list = '刘德华' in names_python_pc
+print('使用 in 判断一个元素是否在列表中', in_list)
+# 使用 not in 判断一个元素不在列表中
+not_in_list = 'fuyao' not in names_python_pc
+print('使用 not in 判断一个元素不在列表中', not_in_list)
 
+'''
+在数据分析中，我们经常需要把两个列表拼接成一个列表。在 Python 中，我们使用加号(+)拼接两个已
+经存在的列表为一个新的列表；
+也可以使用 extend 方法向已存在的列表中添加另一个列表。
+注意第一种方法相对代价高，推荐使用 extend 方法拼接两个存在的列表。
+除了拼接列表以外，我们有时候还需要复制多份同样元素的列表。
+在 Python 中，我们使用星号(*)重复同样元素的列表多份
+'''
+# + 连接
+names_python_pc = ['a', 'c', 'c', 'f']
+number = [2, 3, 5, 7]
+linked_list = names_python_pc + number
+print('连接后的列表:', linked_list)
 
+# 用extend方法向以存在的列表添加多个元素
+names_python_pc.extend(number)
+print('连接后的列表:{}'.format(names_python_pc))
 
+# 使用 * 重复列表元素
+number = number * 2
+print('使用*重复列表元素：', number)
 
+# 列表的高级用法
+# 列表切片是处理列表的部分元素，也就是把整个列表切开。它是整个列表中的重点内容，
+# 我们需要注意的是 Python 中符合序列的有序序列都支持切片（slice），例如列表，字符串，元组。
+'''
+语法：
+ [start:end:step]
+ start:起始索引，从 0 开始
+ end：结束索引,但是 end-1 为实际的索引值
+ step：步长，步长为正时，从左向右取值。步长为负时，反向取值
+ 注意切片的结果不包含结束索引，即不包含最后的一位，-1 代表列表的最后一个位置索引
+'''
+name_fuyao = ['扶摇', '周叔', '国公', '无级太子', '医圣', '非烟殿主', '穹苍']
+# 指定开始和结束位置，注意不包括最后的位置元素
+print('扶摇电视列表中第三个到第五个人物的名字:', name_fuyao[2:5])
 
+# 不指定开始的位置，则默认从头开始
+print('扶摇电视列表中前 5 个人物名字:', name_fuyao[:5])
 
+# 不指定结束的位置，则从开始位置到结束
+print('扶摇电视列表从 6 个人物的名字:', name_fuyao[5:])
 
+# 开始和结束位置都不指定
+print('扶摇电视列表中的名字:', name_fuyao[:])
 
+# 负数索引表示返回距离列表末位相应距离的元素，也就是取列表中后半部分的元素
+print('扶摇电视列表中最后三个人物的名字:', name_fuyao[-3:])
 
+# 取偶数位置的元素
+print('扶摇电视列表中偶数位置的人物是:', name_fuyao[::2])
 
+# 取奇数位置的元素
+print('扶摇电视列表中奇数位置的人物是:', name_fuyao[1::2])
 
+# 逆序列表，相当于 reversed(list)
+print('扶摇电视列表中人物颠倒顺序:', name_fuyao[::-1])
+
+# 在某个位置插入多个元素
+# 也可以用同样的方法插入或者删除多个元素
+name_fuyao[3:3] = ['玄机', '太渊', '天煞'] # 在索引3个位置加入3个元素
+print('扶摇电视列表中人物变为:', name_fuyao)
+# 复制列表,相当于 copy（）,复制以后的新列表是一个新的，可以对其操作
+# 注意如果 new_name_fuyao = name_fuyao 是变量赋值，也就是同一个值给了两个变量，一个改变了值 ，另外的两个级跟着修改了。
+new_name_fuyao = name_fuyao[:]
+print('新的列表元素:{}'.format(new_name_fuyao))
+
+# 列表的其他常用用法
+# list()函数:可以用来定义列表，比如把 hello，变成列表.
+# list(‘str')
+# 把字符串hello变成列表结构： ['h', 'e', 'l', 'l', 'o']
+print('把字符串hello变成列表结构：', list('hello'))
+
+# 在数据分析中，我们经常使用 list()函数将迭代器或生成器转化为列表
+# 使用range生产5个数值，然后变成列表
+number = range(5)
+print(number)
+number_list = list(number)
+print('使用list()函数生产列表', number_list)
+print('number的类型是', type(number_list))
+
+# join() 方法用于将序列中的元素以指定的字符连接生成一个新的字符串
+# 语法 字符串名字.join(列表名字)
+linked = '-'
+data_list = ['Python', 'is', 'NO.1', 'Programing Language']
+print(linked.join(data_list))
+print(type(linked.join(data_list)))
+
+# index 方法：返回元素所在的索引位置
+# 语法 列表名.index('元素名')
+L1 = ['a', 'a', 'b', 'c', 'd', 'e', 'f', 'g']
+print('B 所在的位置', L1.index('b'))
+
+# len()函数计算列表的长度
+# 语法 len(列表名)
+print('L1的列表长度为:', len(L1))
